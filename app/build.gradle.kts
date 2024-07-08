977a5cfc5e4bfdc9db208ad6cc4e2727500c0ced
Dal-Pi <kimsh707@naver.com>
2024-07-09 02:12:36 +0900

    add car ui focus views

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.kania.focusplayground"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
